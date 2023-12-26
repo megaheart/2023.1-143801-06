fc8408354c1a4c57041038e31f6cbc8b5aba923c
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2023-12-26 08:10:09 +0700

    update view

--- Homework07/Projects/PMChamCong/src/main/java/com/groupsix/pages/officerdepartmentattendancereport/OfficerDepartmentAttendanceReportController.java
+++ Homework07/Projects/PMChamCong/src/main/java/com/groupsix/pages/officerdepartmentattendancereport/OfficerDepartmentAttendanceReportController.java
@@ -1,20 +1,44 @@
 package com.groupsix.pages.officerdepartmentattendancereport;
 
+import com.groupsix.attendance.AttendanceFactory;
+import com.groupsix.hrsubsystem.Department;
+import com.groupsix.hrsubsystem.HRSubsystemFactory;
 import com.groupsix.report.OfficerAndAttendance;
 import com.groupsix.report.OfficerAttendanceReport;
+import com.groupsix.report.ReportHelper;
 import com.groupsix.user.User;
 import com.groupsix.hrsubsystem.Employee;
 import com.groupsix.attendance.OfficerAttendance;
+import com.groupsix.user.UserService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import java.io.File;
+
 public class OfficerDepartmentAttendanceReportController {
+	private OfficerDepartmentAttendanceReportView view;
 
 	public OfficerDepartmentAttendanceReportController(OfficerDepartmentAttendanceReportView view) {
-
+		this.view = view;
+		view.setOnTimeRangeChangedHandler((e) -> {
+			var timeRange = view.getTimeRange();
+			var month = timeRange.getMonth();
+			var year = timeRange.getYear();
+			var monthCount = timeRange.getMonthCount();
+			getReport(department, month, year, monthCount);
+		});
+		view.setOpenExportPanelHandler((e) -> {
+			showExportPanel();
+		});
+		view.setOnEmployeeChooseHandler((sender, e) -> {
+			var timeRange = view.getTimeRange();
+			openEmployeeView(e, timeRange.getMonth(), timeRange.getYear(), timeRange.getMonthCount());
+		});
+		init();
 	}
 
-	private int department;
+	private Department department;
 
 	private OfficerAttendanceReport report;
 
@@ -24,20 +48,75 @@ public class OfficerDepartmentAttendanceReportController {
 
 	private AttendanceReportExportPanel exportPanel;
 
+	private void init() {
+		var user = UserService.getInstance().getCurrentUser();
+		var employeeRepo = HRSubsystemFactory.getInstance().createEmployeeRepository();
+		var employeeOfUser = employeeRepo.getEmployeeByCode(user.getEmployeeCode());
+		var _department = HRSubsystemFactory.getInstance().createDepartmentRepository()
+				.getDepartmentByCode(employeeOfUser.getDepartmentCode());
+		this.department = _department;
+
+		var now = LocalDate.now();
+
+		getReport(_department, now.getMonthValue(), now.getYear(), 1);
+	}
+
 	public void exportReportAsFile(OfficerAttendanceReport report, String path, String format) {
 
 	}
 
-	public ArrayList<OfficerAndAttendance> mergeOfficerAndAttendance(int officers, ArrayList<OfficerAttendance> officerAttendances) {
-		return null;
+	public OfficerAndAttendance mergeOfficerAndAttendance(Employee officer, ArrayList<OfficerAttendance> officerAttendances) {
+		OfficerAndAttendance officerAndAttendance = new OfficerAndAttendance();
+		officerAndAttendance.setEmployee(officer);
+		officerAndAttendance.setFullName(officer.getFullName());
+		officerAndAttendance.setDepartmentName(this.department.getDepartmentName());
+
+		var totalSession = 0;
+		var totalHoursNotWork = 0.0;
+
+		for(var attendance : officerAttendances) {
+			if(attendance.isMorningSession()) {
+				totalSession++;
+			}
+
+			if(attendance.isAfternoonSession()) {
+				totalSession++;
+			}
+
+			totalHoursNotWork += attendance.getHoursEarlyLeave() + attendance.getHoursLate();
+		}
+
+		officerAndAttendance.setTotalSession(totalSession);
+		officerAndAttendance.setHoursNotWork(totalHoursNotWork);
+
+		return officerAndAttendance;
 	}
 
-	public OfficerAttendanceReport getReport(int department, int month, int year, int monthCount) {
-		return null;
+	public OfficerAttendanceReport getReport(Department department, int month, int year, int monthCount) {
+		var employeeRepo = HRSubsystemFactory.getInstance().createEmployeeRepository();
+		var departmentRepo = HRSubsystemFactory.getInstance().createDepartmentRepository();
+		var attendanceRepo = AttendanceFactory.getInstance().createRepository();
+
+		var officerList = employeeRepo.getEmployeesInDepartment(department);
+		var mergedOfficerAttendances = new ArrayList<OfficerAndAttendance>();
+
+		for(var officer : officerList) {
+			var officerAttendances = attendanceRepo.getAttendancesOfEmployee(user, officer, month, year, monthCount);
+			var mergedOfficerAttendance = mergeOfficerAndAttendance(officer, officerAttendances);
+			mergedOfficerAttendances.add(mergedOfficerAttendance);
+		}
+
+		var report = ReportHelper.summarizeReport(department, mergedOfficerAttendances, month, year, monthCount);
+		this.report = report;
+
+		view.show(report);
+
+		return report;
 	}
 
 	public boolean checkPathExist(String path) {
-		return false;
+		File f = new File(path);
+		return f.exists() && !f.isDirectory();
 	}
 
 	public void showExportPanel() {
@@ -48,7 +127,7 @@ public class OfficerDepartmentAttendanceReportController {
 
 	}
 
-	public void openEmployeeView(Employee employee, int month, int year) {
+	public void openEmployeeView(Employee employee, int month, int year, int monthCount) {
 
 	}
 
